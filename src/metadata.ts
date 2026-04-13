878dc458e7c52876f22fa627867eea2a844ec830
Jan Slifka <jan.slifka@gmail.com>
2026-04-13 15:17:28 +0200

    Update README and version

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,8 +1,10 @@
 import { PluginMetadata } from '@ds-wizard/plugin-sdk/types'
 
+import { version } from '../package.json'
+
 export const pluginMetadata: PluginMetadata = {
     uuid: '<uuid>',
     name: 'DSW Plugin',
-    version: '1.0.0',
     description: 'This is a DSW Plugin created from the template.',
+    version,
 }
